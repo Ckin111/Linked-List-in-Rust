cb47db67944a460af850b213879c1a919a235916
Ckin111 <niccollsb@gmail.com>
2022-05-12 00:04:10 -0400

    starting day 2 of learning

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,15 @@
+/* 
+Primitive Types--
+Integers: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128 (number of bits they take in memory) (u means unsigned, not positive or negative)
+Floats: f32, f64
+Boolean (bool)
+Characters (char)
+Tuples
+Arrays
+*/
+
+// Rust is statically typed language
+
 pub fn run(){
-    
+
 }
