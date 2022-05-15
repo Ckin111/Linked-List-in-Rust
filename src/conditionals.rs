9439ba247511a43de15e2b89bd473b11286660d9
Ckin111 <niccollsb@gmail.com>
2022-05-15 17:19:22 -0400

    couple things added

---
+++ src/conditionals.rs
@@ -0,0 +1,5 @@
+// Conditionals are used to check the conditon of something and act on the result
+
+pub fn run(){
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::vec;
+
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+mod conditionals;
 fn main() {
     // print::run();
     // println!("Hello, world!");
     // vars::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    // tuples::run();
+    // arrays::run();
+    // vectors::run();
+    conditionals::run();
 }
